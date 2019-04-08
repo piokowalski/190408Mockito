c22067926f827c9cb1074d5066993f243927291f
piokowalski <piokowalski@onet.pl>
2019-04-08 10:04:23 +0200

    Task 1 done - mocking list of users

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,14 +1,14 @@
-import org.junit.jupiter.api.BeforeAll;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.awt.*;
-
-import static org.mockito.Mockito.mock;
 
 //@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -36,7 +36,20 @@ public class UserServiceTest {
 
     @Test
     public void shouldGetAllUsers() {
-        // Zadanie 1
+
+
+        when(userDao.getAllUsers()).thenReturn(List.of(
+                new User("Piotrek"),
+                new User("Remik"),
+                new User("Arek"),
+                new User("Artur")
+        ));
+
+        // when
+        List<User> result = userService.getAllUsers();
+
+        // then
+        assertThat(result).hasSize(4);
     }
 
     @Test
